79a989f93b6358a421f9f4f5bf35fdb8faabe3d6
equitog <lycan_nirva911@hotmail.com>
2019-07-04 22:44:31 -0500

    Version 1.0

--- eqweb/eq_datascience/urls.py
+++ eqweb/eq_datascience/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.show_data, name='show_data'),
 ]
--- eqweb/eq_datascience/views.py
+++ eqweb/eq_datascience/views.py
@@ -7,4 +7,13 @@ def index(request):
     return render(
         request,
         'index.html',
+        context={'num_test': 10}
+    )
+
+
+def show_data(request):
+    return render(
+        request,
+        'show_data.html',
+        context={'num_test': 10}
     )
